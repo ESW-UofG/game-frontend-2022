78e4bb8d76bf4a973c86a84ecb742e373971225b
Ian McKechnie <imckechn@uoguelph.ca>
2022-11-15 18:12:59 -0500

    Minor updates

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -55,9 +55,25 @@ function Homepage(){
                     </tr>
                 </tbody>
             </table>
+            
+            <br/>
+            <br/>
+            <div>
+                <h2> How to contact us! </h2>
+                <h5> Email: </h5>
+                <h5> Meeting Details: Thornborough Building, Room 1002 Every Tuesday at 6:00pm</h5>
+            </div>
+
+            <div>
+                <h2> Locations of other ESW Arcade Games! </h2>
+            </div>
         </div>
 
-        /*Add in contact details for club so people can sign up */
+        /**
+         * Add in contact details for club so people can sign up
+         * Add in info about why we are collecting the pop can tabs
+         * Add in locations of the other arcade games
+        */
     );
 };
 
